489aae8323c9b185842215dcc4ace4d7f3806b8d
icon <jiaxiangting@socmap.net>
2021-03-12 17:19:09 +0800

    使用发包

--- review/blog/src/pages/Article/h5.jsx
+++ review/blog/src/pages/Article/h5.jsx
@@ -1,14 +1,16 @@
 /**
  * Created by icon on 2021/3/12
  */
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useRequest } from 'ahooks';
 import { Divider } from 'antd';
 import { articleInfo } from './services';
 import 'react-quill/dist/quill.snow.css';
+import { browserRedirect } from '@/utils/utils';
 import styles from './h5.less';
 
 const Index = (props) => {
+  const [ua, setUa] = useState('pc');
   const { run, data, loading } = useRequest(articleInfo, {
     manual: true,
     onSuccess() {
@@ -18,12 +20,14 @@ const Index = (props) => {
   });
   useEffect(() => {
     run(props.match.params.id);
+    judgeUa();
+    window.addEventListener('resize', judgeUa);
     return () => {
-
+      window.removeEventListener('resize', judgeUa);
     }
   }, []);
-  const onClick = () => {
-    downLoad(saveAsPNG(document.getElementById('qrcode')))
+  const judgeUa = () => {
+    setUa(browserRedirect())
   };
   //模拟a标签href下载
   const downLoad = (url) => {
@@ -41,7 +45,7 @@ const Index = (props) => {
   if (loading) return null;
   return (
     <div className={styles.box}>
-      <div className={`ql-container ql-snow ${styles.articleContainer}`}>
+      <div className={`ql-container ql-snow ${styles.articleContainer}  ${ua === 'phone' ? styles.mobile : ''}`}>
         <h2 style={{fontWeight: 'bold'}}>{data?.title}</h2>
         <Divider />
         <div className="ql-editor" dangerouslySetInnerHTML={{__html: data?.content}}/>
